0f423f077c9f26d1899be001cea0311103e5bef5
Monica <monica.walentek@gmail.com>
2022-05-11 22:27:20 +0200

    filter the case that can t be played

--- components/BoardGame/BoardGame.js
+++ components/BoardGame/BoardGame.js
@@ -19,11 +19,11 @@ export default function BoardGame() {
         })
     }, []);
 
-    useEffect(()=>{
+    /* useEffect(()=>{
         console.log(state)
-    },[state])
+    },[state]) */
 
-    const [listIdNotUsed, setListIdNotUsed] = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+    const [listIdNotUsed, setListIdNotUsed] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
 
     const retry = () => {
         getRandomColor().then(res => {
@@ -34,25 +34,37 @@ export default function BoardGame() {
             setState(res);
 
         })
-        //setListIdNotUsed(['0', '1', '2', '3', '4', '5', '6', '7', '8']);
+        setListIdNotUsed([0, 1, 2, 3, 4, 5, 6, 7, 8]);
     }
 
     const [board, setBoard] = useState([]);
 
     const onPressUser = (id) => {
+        //first, we modify the color name by the cross and set State by the new state
         const newState = state.map((array) => {
             if (array.id === id) array.color_name = '❌'
             return array
         })
-
         setState(newState);
-        console.log(state[id].color_name);
+
+        // if(listIdNotUsed[id] == id.toString()) {
+
+        //then, we filter ListIdNotUsed of the id which has the color name changed.
+        const newListIdNotUsed = listIdNotUsed.filter((value) => { return value !== id })
+        console.log(newListIdNotUsed)
+        setListIdNotUsed(newListIdNotUsed)
+        console.log(listIdNotUsed);
+        //The random could take one number of this array and put a circle.
+        //si la longueur n'est pas inférieur à 2
+        const random = Math.floor(Math.random() * newListIdNotUsed.length);
+        const numberChooseByRandom = newListIdNotUsed[random];
+        console.log(numberChooseByRandom);
     }
 
     const pushValueUser = (id) => {
         setBoard.push([id, '❌'])
         // put the text related to the id on the board.
-        setListIdNotUsed(listIdNotUsed.filter(x => (x == id)))
+        setListIdNotUsed(listIdNotUsed.filter(x => (x == id.toString())))
     }
 
     const pushValueRandom = () => {
@@ -93,7 +105,7 @@ export default function BoardGame() {
                             alignItems: 'center',
                             backgroundColor: item.hex_value
                         }}
-                        onPress={() => onPressUser(item.id)}>
+                            onPress={() => onPressUser(item.id)}>
                             <Text>{item.color_name}</Text>
                         </Pressable>
                     }
